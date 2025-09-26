a0ad087340ec9a5135d86a2518f502f301199f3a
beansebrrr <173890355+beansebrrr@users.noreply.github.com>
2025-09-27 00:21:30 +0400

    Prepare `Student` for easier UI integration

--- 03--student-manager/student.py
+++ 03--student-manager/student.py
@@ -1,24 +1,26 @@
-from pathlib import Path
 from textwrap import dedent
-    
+
+
+COURSE_MARKS_MAX = 20
+EXAM_MARKS_MAX = 100
+NUM_OF_COURSES = 3
+
 
 class Student:
-    numOfCourses = 3
-    courseMarksMax = 20
-    examMarksMax = 100
+
     def __init__(self, idNum: str, name: str, courseMarks: list[int], examMarks: int):
         # Guard clauses
         if len(idNum) != 4 or not idNum.isdecimal():
             raise ValueError("Student's ID must be a 4-digit number")
-        if len(courseMarks) != self.numOfCourses:
+        if len(courseMarks) != NUM_OF_COURSES:
             raise ValueError("Course marks should only have 3 items")
         self.idNum = idNum
         self.name = name
         self.courseMarks = courseMarks
         self.examMarks = examMarks
 
-    def announceInfo(self):
-        """Prints out student's information into the console."""
+    def info(self):
+        """Returns a string of student's information."""
         # `dedent()` is just used for cleaner code formatting
         infoString = dedent(f"""\
             Name: {self.name}
@@ -33,11 +35,11 @@ class Student:
             Exam marks: {self.examMarks}
             Final Grade: {self.generalAverage():.2%} | {self.grades()}
         """)
-        print(infoString)
+        return infoString
 
     def generalAverage(self):
         totalMarks = sum(self.courseMarks) + self.examMarks
-        maxMarks = self.examMarksMax + (self.courseMarksMax * self.numOfCourses)
+        maxMarks = EXAM_MARKS_MAX + (COURSE_MARKS_MAX * NUM_OF_COURSES)
         genAverage = round(totalMarks / maxMarks, 4)
         return genAverage
     
@@ -45,11 +47,11 @@ class Student:
         generalAverage = self.generalAverage() * 100
         if generalAverage >= 70.00:
             return "A"
-        elif 69.99 >= generalAverage >= 60.00:
+        elif 70 > generalAverage >= 60.00:
             return "B"
-        elif 59.99 >= generalAverage >= 50.00:
+        elif 60 > generalAverage >= 50.00:
             return "C"
-        elif 49.99 >= generalAverage >= 40.00:
+        elif 50 > generalAverage >= 40.00:
             return "D"
         else:
             return "F"
